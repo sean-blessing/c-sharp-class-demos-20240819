0fb0f3860971f6181ea677b0f2fa37b9c5928c21
Sean Blessing <snblessing@gmail.com>
2024-08-27 11:12:38 -0400

    init commit

---
+++ Sandbox/Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sandbox {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to my sandbox. Let's play!");
+
+            // Holly question 8/27 int vs Int32
+            int a = 5;
+            Int32 b = 20;
+
+
+
+
+
+            Console.WriteLine("bye");
+        }
+    }
+}
