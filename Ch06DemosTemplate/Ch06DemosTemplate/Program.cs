02cc27468013b2abe2866b2e85ec3b3e62ecb6de
Sean Blessing <snblessing@gmail.com>
2024-08-28 15:25:09 -0400

    updated to include template comments

--- Ch06DemosTemplate/Ch06DemosTemplate/Program.cs
+++ Ch06DemosTemplate/Ch06DemosTemplate/Program.cs
@@ -1,7 +1,34 @@
 ﻿namespace Ch06DemosTemplate {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Chapter 6 Demos!");
+
+            // p. 171 Coding a method
+            // no params and no return
+            // one param and no return
+            // one param and return a decimal
+            // three params and return a decimal
+
+            // p. 173 Calling a Method
+
+            // p. 175 Optional Params
+
+            // p. 177 Replacing Code w/ a Method via Refactor
+
+            // p. 181 ref Keyword
+
+            // p. 183 out, in Keywords
+
+            // p. 185 Tuples, p. 187 more on Tuples
+
+            // p. 189 - skip the rest
+
+
+
+
+
+
+            Console.WriteLine("Bye");
         }
     }
 }
