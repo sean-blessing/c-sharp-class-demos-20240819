8508868c5815aa288ebef6c389d55e4fa8c4be51
Sean Blessing <snblessing@gmail.com>
2024-08-30 15:53:41 -0400

    ch12 class demo

---
+++ Ch12ClassesDemo/Ch12ClassesDemo/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Ch12ClassesDemo {
+    public class Product {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        // don't use objectCount... demo purposes only!!!
+        static public int ObjectCount { get; set; }
+
+        public Product() {
+            ObjectCount++;
+        }
+
+        public Product(string code, string description, decimal price) {
+            this.Code = code;
+            this.Description = description;
+            this.Price = price;
+            ObjectCount++;
+        }
+
+        public override string ToString() {
+            return $"Product: Code - '{this.Code}', Description - '{this.Description}', Price - {this.Price}, " +
+                $"ObjectCount - {Product.ObjectCount}";
+        }
+
+
+
+    }
+}
--- Ch12ClassesDemo/Ch12ClassesDemo/Program.cs
+++ Ch12ClassesDemo/Ch12ClassesDemo/Program.cs
@@ -3,6 +3,70 @@
         static void Main(string[] args) {
             Console.WriteLine("Welcome to the Classes Demo App!");
 
+            // define a class
+            // use the class
+
+            // define an integer named 'number' and assign a value of 5
+            int number = 5;
+
+            // define a Product named 'p1' and assign values...
+            Product p1 = new Product();
+            p1.Code = "c#";
+            p1.Description = "Murach's C# Programming";
+            p1.Price = 57.5m;
+
+            Console.WriteLine($"number: {number}");
+            Console.WriteLine($"p1: {p1}");
+            Console.WriteLine($"p1 properties: code - {p1.Code}, description - {p1.Description}, price - {p1.Price}");
+
+            p1.Price = 49.99m;
+
+            Console.WriteLine($"p1 properties: code - {p1.Code}, description - {p1.Description}, price - {p1.Price}");
+
+            Product p2 = new Product("java", "Murach's Java Programming", 59.50m);
+            Console.WriteLine($"p2: {p2}");
+
+            Product p3 = new Product("andr", "Murach's Android Programming", 62.50m);
+            Console.WriteLine($"p3: {p3}");
+
+            Console.WriteLine("pass p1 directly to WriteLine: " + p1.ToString());
+            Console.WriteLine(p1);
+
+            List<Product> products = new List<Product>();
+            
+            products.Add(p1); 
+            products.Add(p2); 
+            products.Add(p3);
+
+            Console.WriteLine("List of products:");
+            foreach (Product p in products) {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine("Find a product by code: ");
+            Console.WriteLine("========================");
+            Console.Write("Enter code to find: ");
+            string code = Console.ReadLine();
+
+            Product foundProduct = null;
+            foreach (Product p in products) {
+                if (p.Code == code) {
+                    foundProduct = p;
+                    break;
+                }
+            }
+
+            if (foundProduct != null) {
+                Console.WriteLine($"Found Product: {foundProduct}");
+            }
+            else {
+                Console.WriteLine($"No product found for code: '{code}'");
+            }
+
+            //Console.WriteLine($"object count p1: {Product.ObjectCount}");
+
+
+
             // p. 385 Method that accepts parameters
 
             // p. 387 Constructors
