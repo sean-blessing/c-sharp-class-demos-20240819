e2c5d4488905fdded4f3f0f13a521b4fbf92d3e3
Sean Blessing <snblessing@gmail.com>
2024-09-10 16:21:30 -0400

    max # checker

---
+++ HackerRankPractice/HackerRankPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HackerRankPractice {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, max number checker!");
+
+            int maxVal = 0;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++) {
+                int nbr = rand.Next(1, 100);
+                Console.WriteLine($"nbr: {nbr}");
+                maxVal = Math.Max(maxVal, nbr);
+            }
+
+            Console.WriteLine($"maxVal {maxVal}");
+
+        }
+
+    }
+}
