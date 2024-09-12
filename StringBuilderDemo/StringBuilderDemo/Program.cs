212fae95f50dd22e4d141ea330d79de3bb1fa068
Sean Blessing <snblessing@gmail.com>
2024-09-12 13:28:06 -0400

    StringBuilder demo

---
+++ StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace StringBuilderDemo {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("String Builder Demo!");
+
+            string str1 = "abc";
+            Console.WriteLine(str1);
+            str1 += "def";
+            Console.WriteLine(str1);
+
+            Console.WriteLine("StringBuilder");
+            StringBuilder strb = new StringBuilder();
+            strb.Append(str1);
+            Console.WriteLine(strb);
+            strb.Remove(2, 2);
+            Console.WriteLine(strb);
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
