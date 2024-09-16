ce2a8d76f32f2fcda58471407e3e6ea2032527f0
Sean Blessing <snblessing@gmail.com>
2024-09-16 13:45:27 -0400

    replacing string parts demo

---
+++ ReplaceCharsInString/ReplaceCharsInString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ReplaceCharsInString {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Let's replace chars in a string!");
+
+            // create a string of asterisks
+            string aStr = "*******************";
+
+            Console.WriteLine($"aStr: {aStr}");
+
+            char[] stringArray = aStr.ToArray();
+            // replace every other * with a + sign
+            for (int i = 0; i < stringArray.Length; i+=2) {
+                stringArray[i] = '+';
+            }
+
+            string newString = new string(stringArray);
+            Console.WriteLine($"stringArray: {newString}");
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
