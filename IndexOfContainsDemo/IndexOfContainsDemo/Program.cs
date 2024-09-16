f54d6b4dcc755116f81ea67978dee8954ab78689
Sean Blessing <snblessing@gmail.com>
2024-09-16 15:39:52 -0400

    demo

---
+++ IndexOfContainsDemo/IndexOfContainsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IndexOfContainsDemo {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Contains/IndexOf Demo!");
+
+            // word guess game example - does a letter occur in the word?
+
+            string theWord = "superstar";
+            Console.WriteLine($"theWord: {theWord}");
+
+            while (true) {
+                Console.Write("Guess a letter: ");
+                string letter = Console.ReadLine();
+                if (letter == "exit") {
+                    break;
+                }
+                // does the letter exist in the word?
+                // indexOf...
+                Console.WriteLine($"theWord.IndexOf(letter): {theWord.IndexOf(letter)}");
+                // contains
+                Console.WriteLine($"theWord.Contains(letter)}}: {theWord.Contains(letter)}");
+
+            }
+
+
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
