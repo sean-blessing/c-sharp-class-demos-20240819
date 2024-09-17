ead69374de0f5b2be5f13d486fcc096a8cfd0503
Sean Blessing <snblessing@gmail.com>
2024-09-17 11:15:41 -0400

    wof plan and pseudocode

---
+++ WheelOfFortuneConsole/WheelOfFortuneConsole/Model/WofPhrases.cs
@@ -0,0 +1,24 @@
+﻿
+namespace WheelOfFortuneConsole.Model {
+    public class WofPhrases {
+        private static List<string> phrases = new List<string> {
+            "Remains of the day", "No time to kill", "Thank god its Friday", "May the force be with you",
+            "Whats up Doc", "Bond James Bond", "What time is it", "A bigger bang for your buck", "A Blessing In Disguise",
+            "A Chip On Your Shoulder", "A Dime A Dozen", "A Piece Of Cake","Actions Speak Louder Than Words",
+            "Barking Up The Wrong Tree", "Beating Around The Bush", "Better Late Than Never", "Bite The Bullet",
+            "Break The Ice", "Burning The Midnight Oil","Caught Red-Handed", "Cost An Arm And A Leg", "Curiosity Killed The Cat",
+            "Don't Count Your Chickens Before They Hatch","Every Cloud Has A Silver Lining", "Fit As A Fiddle",
+            "Hit The Nail On The Head","It's Raining Cats And Dogs","Let The Cat Out Of The Bag", "On Cloud Nine",
+            "Once In A Blue Moon","Piece Of The Pie", "Sit Tight", "The Ball Is In Your Court"
+        };
+
+        public static string GetPhrase() {
+            Random rand = new Random();
+            int idx = rand.Next(phrases.Count());
+            string phrase = phrases[idx];
+            // removing from list so we won't play the same phrase twice
+            phrases.RemoveAt(idx);
+            return phrase.ToUpper();
+        }
+    }
+}
---
+++ WheelOfFortuneConsole/WheelOfFortuneConsole/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace WheelOfFortuneConsole {
+    internal class Program {
+        // initialize global variables
+        // Note: tech decision made: storing values as strings, but these could be ints
+        static List<string> wheelValues = new List<string> {
+            "bankrupt", "lose a turn", "100", "150", "200", "250", "300", "350",
+            "400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900"
+        };
+        // do we need an overallBank? Could just add roundBanks.
+        decimal overallBank = 0.0m;
+        decimal[] roundBanks = { 0.0m, 0.0m, 0.0m};
+
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Wheel of Fortune Game!\n");
+            Console.WriteLine("Instructions go here. Do this later\n");
+            //initialize local variables
+            // do variables go here, or at the class level?
+
+            string choice = "Y";
+            while (choice == "Y") {
+                // what's a 'game'? ==> It's one round of wof
+                // - initialize single game local variables
+
+                for (int i = 0; i < 3; i++) {
+                    // ** Beginning of a Round!
+                    // Display "welcome to round {i}" message
+                    // initialize the bank (set a variable to track the roundBank)
+                    // get the 'roundPhrase'
+                    // generate 'hiddenPhrase'
+                    // display 'hiddenPhrase' (inside while or outside?)
+                    // while loop for gameplay (loop until user guesses the phrase)
+                        // prompt (spin, buyVowel, or guessPhrase)
+                        // - if spin
+                        //   - spin wheel
+                        //      - bankrupt - zero roundBalance, continue
+                        //      - lose a turn - continue
+                        //      - $$ amount, guess letter
+                        //          - check 'roundPhrase' for letter
+                        //              - if exists, resolve letter in 'hiddenPhrase'
+                        //                  - add $$ to roundBank
+                        //              - if not exist, continue
+                        //   - buyVowel
+                        //      - if enough $$ prompt for vowel
+                        //          deduct from roundBank
+                        //          if vowel exists, resolve in word
+                        //          if vowel not exists, continue
+                        //   - guessPhrase
+                        //      - prompt for guess
+                        //      - if correct win
+                        //      - else continue
+                        //   - round ends when 'roundPhrase' is guessed, ends this iteration of for loop
+                }
+                choice = PromptString("Play again? ", ["Y", "N"]);
+            }
+
+            Console.WriteLine("Bye");
+        }
+
+        // Validation Methods?
+        private static string PromptString(string prompt, string[] validStrings) {
+            string goodString = "";
+            Boolean isValid = false;
+            while (!isValid) {
+                Console.Write(prompt);
+                goodString = Console.ReadLine().ToUpper();
+                foreach (string s in validStrings) {
+                    if (s == goodString) {
+                        isValid = true;
+                        break;
+                    }
+                }
+            }
+
+            return goodString;
+        }
+    }
+}
