0592939bf1620a3c4291da91c89a61ad2d1962ca
Sean Blessing <snblessing@gmail.com>
2024-09-18 15:15:23 -0400

    nullable types demo

---
+++ NullableTypesConsole/NullableTypesConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace NullableTypesConsole {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the nullable types demo!");
+
+            // string defined as an empty string - not null!
+            string str = "";
+            Console.WriteLine($"str = {str}.");
+
+            string? nullString  = null;
+            Console.WriteLine($"nullString = {nullString}.");
+
+            //Console.WriteLine($"nullString.ToUpper() = {nullString.ToUpper()}.");
+
+            int a = 0;
+            int? b = null;
+
+            Console.WriteLine($"a, b = {a}, {b}.");
+            Console.WriteLine($"a + b = {a + b}.");
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
