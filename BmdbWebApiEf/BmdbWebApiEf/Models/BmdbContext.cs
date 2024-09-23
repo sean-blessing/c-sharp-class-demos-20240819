464b69188195199e46c8f7ba04187b1657655fe7
Sean Blessing <snblessing@gmail.com>
2024-09-23 12:53:00 -0400

    init commit

---
+++ BmdbWebApiEf/BmdbWebApiEf/Models/BmdbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BmdbWebApiEf.Models;
+
+public partial class BmdbContext : DbContext
+{
+
+    public BmdbContext(DbContextOptions<BmdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Actor> Actors { get; set; }
+
+    public virtual DbSet<Credit> Credits { get; set; }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+}
