189fa90a3af5ad6a49ac61db02dba64cb6842e68
Sean Blessing <snblessing@gmail.com>
2024-09-27 15:07:47 -0400

    moved bmdb app, adding request # generator projects

---
+++ PrsRequestNumberCreator/PrsRequestNumberCreator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace PrsRequestNumberCreator {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Request Number Creator!");
+            Console.WriteLine("These #s will be based on the current date.\n");
+
+            Console.WriteLine("The format for a request # is 'R{YYMMDD###}', where '###' is the 9 digit #:");
+            Console.WriteLine("Here are 10 request #s for today: ");
+            int requestCounter = 0;
+            for (int i = 0; i < 10; i++) {
+                StringBuilder requestNbr = new StringBuilder("R");
+                string dateStr = String.Format("{0:yyMMdd}", DateTime.Now);
+                //string yy = today.Year.ToString().Substring(2);
+                //string mm = today.Month.ToString().PadLeft(2, '0');
+                //string dd = today.Day.ToString().PadLeft(2, '0');
+                requestCounter++;
+                string count = requestCounter.ToString().PadLeft(3, '0');
+                requestNbr.Append(dateStr).Append(count);
+                Console.WriteLine($"Request[{i}]: {requestNbr}");
+            }
+            
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ PrsRequestNumberGeneratorV02/PrsRequestNumberGeneratorV02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace PrsRequestNumberGeneratorV02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Request Number Generator!");
+            // Request Number Biz Requirements:
+            // R2409270001
+            // 'R' - first letter
+            // 2 digit year
+            // 2 digit month
+            // 2 digit day
+            // 4 digit counter
+
+            // generate 10 request numbers
+            int counter = 0;
+            for (int i = 0; i < 10; i++) {
+                counter++;
+                DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+                string yy = today.Year.ToString().Substring(2);
+                string mm = today.Month.ToString().PadLeft(2, '0');
+                string dd = today.Day.ToString().PadLeft(2, '0');
+                string counterStr = counter.ToString().PadLeft(4, '0');
+                StringBuilder requestNumber = new StringBuilder("");
+                requestNumber.Append("R");
+                // append yy
+                requestNumber.Append(yy);
+                // append mm
+                requestNumber.Append(mm);
+                // append dd
+                requestNumber.Append(dd);
+                // append ####
+                requestNumber.Append(counterStr);
+                Console.WriteLine(requestNumber);
+            }
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
